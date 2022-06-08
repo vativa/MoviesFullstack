68760d36f1179275c92b4737436edc46c69b5664
Tichomir Rangelov <tichomirr@lightds.de>
2022-06-08 17:35:58 +0200

    Ejecting react app

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -27,13 +27,15 @@ namespace MoviesFullstack.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            WeatherForecast[] items = Enumerable.Range(1, 5).Select((int index) => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+
+            return items;
         }
     }
 }
